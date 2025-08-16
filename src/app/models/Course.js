9e2e3b918a831fedc8001dc5d6bd65e980b836b9
Tunhoclaptrinh <tuannguyentien16@gmail.com>
2025-08-16 18:41:27 +0700

    npm install mongoose-slug-updater

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,18 +1,19 @@
-const mongoose = require('mongoose')
-const slug = require('mongoose-slug-generator')
+const mongoose = require('mongoose');
+const slug = require('mongoose-slug-updater');
+
 mongoose.plugin(slug);
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
 const Course = new Schema({
-  name: { type: String, require: true },
+  name: { type: String, required: true },
   description: { type: String },
   image: { type: String },
-  videoId: { type: String, require: true },
+  videoId: { type: String, required: true },
   level: { type: String },
   slug: { type: String, slug: 'name', unique: true },
 }, {
   timestamps: true
-})
+});
 
-module.exports = mongoose.model('Course', Course)
+module.exports = mongoose.model('Course', Course);
