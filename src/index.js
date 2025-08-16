c1a436388041004c1378b96f48a99ecb194a0920
Tunhoclaptrinh <tuannguyentien16@gmail.com>
2025-08-16 13:15:16 +0700

    [CRUD] Update course

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const path = require('path')
 const express = require('express')
 const morgan = require('morgan')
+const methodOverride = require('method-override');
 const { engine } = require('express-handlebars');
+
 const app = express()
 const port = 3000
 
@@ -17,11 +19,18 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+app.use(methodOverride('_method'));
+
 // HTTP logger
 app.use(morgan('combined'))
 
 // Template engine
-app.engine('hbs', engine({ extname: '.hbs' }));  // Dùng engine()
+app.engine('hbs', engine({
+  extname: '.hbs',
+  helpers: {
+    sum: (a, b) => a + b,
+  },
+}));  // Dùng engine()
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources', 'views')); // Thư mục views
 
